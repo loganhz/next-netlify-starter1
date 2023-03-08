d3f45b643bd22e9c12007ec3f69460751f9be1e2
灵轮 <linglun.zh@alibaba-inc.com>
2023-03-08 15:42:04 +0800

    x

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,7 @@ export default function Home() {
         <p
           className="description"
           onClick={() => {
+            console.log("xxxx");
             netlifyIdentity.open();
           }}
         >
